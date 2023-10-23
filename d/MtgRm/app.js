86eb659a3e6b18a83fd475ecf96638105e42a977
S. Erisman <github@serisman.com>
2023-10-22 23:26:50 -0500

    Better subscription topic parsing

--- d/MtgRm/app.js
+++ d/MtgRm/app.js
@@ -1,20 +1,23 @@
-// Documentation:
-//   https://eclipse.dev/paho/index.php?page=clients/js/index.php
-//   https://eclipse.dev/paho/files/jsdoc/index.html
-
 let _mqttCreds;
-let _mqttClient;
 let _shouldReconnect;
 
 const mics = ["nw", "ne", "sw", "se"];
 const spks = ["nw", "ne", "mw", "me", "sw", "se"];
 
 function onMQTTConnected() {
-  $("#statusPanel").hide();
   if (_shouldReconnect) {
     setStatus("Connected");
-    _mqttClient.subscribe("MtgRm/+/stat/+", {qos: 0});
     loadMainView();
+
+    MQTT_Subscribe("MtgRm/mic/stat/+", (status, [mic]) => {
+      // status: enabled, active, muted
+      $(`#mic-${mic}`).removeClass(["pending", "enabled", "active", "muted"]).addClass(status);    
+    });
+    MQTT_Subscribe("MtgRm/spk/stat/+", (status, [spk]) => {
+      // status: enabled, active
+      $(`#spk-${spk}`).removeClass(["pending", "enabled", "muted"]).addClass(status);    
+    });
+
   } else {
     MQTT_Disconnect();
   }
@@ -39,33 +42,12 @@ function onMQTTConnectionLost(reason) {
   $("#statusPanel").show();
 }
 
-function onMQTTMessage(topic, payload) {
-  //console.log(topic, payload);
-  if (topic.startsWith("MtgRm/mic/stat/")) {
-    const mic = topic.replace("MtgRm/mic/stat/", "");
-    // enabled, active, muted
-    $(`#mic-${mic}`).removeClass(["pending", "enabled", "active", "muted"]).addClass(payload);    
-  }
-  if (topic.startsWith("MtgRm/spk/stat/")) {
-    const spk = topic.replace("MtgRm/spk/stat/", "");
-    // enabled, active
-    $(`#spk-${spk}`).removeClass(["pending", "enabled", "muted"]).addClass(payload);    
-  }
-}
-
-function numPadToX(num, X) {
-  return num.toString().padStart(X,'0');
-}
-
-function roundToX(num, X) {
-  return +(Math.round(num + "e+" + X) + "e-" + X);
-}
-
 function setStatus(text) {
   $('#status').text(text);
 }
 
 function loadMainView() {
+  $("#statusPanel").hide();
   $("#ConnectionView").hide();
   $("#MainView").show();
   $("#btnBar").show();
@@ -90,47 +72,7 @@ function loadConnectionView() {
   $('link[rel="icon"]').attr('href', "icons/32/connect.png");
 }
 
-function MQTT_Publish(topic, payload, qos=0, retained=false) {
-  const message = new Paho.MQTT.Message(payload);
-  message.destinationName = topic;
-  message.qos = qos;
-  message.retained = retained;
-  _mqttClient.send(message);
-}
-
-function MQTT_Connect() {
-  if (!_shouldReconnect) return;
-
-  _mqttClient = new Paho.MQTT.Client(_mqttCreds.host, 443, "myclientid_" + parseInt(Math.random() * 100, 10));
-  _mqttClient.onConnectionLost = onMQTTConnectionLost;
-
-  _mqttClient.onMessageArrived = function(message) {
-    onMQTTMessage(message.destinationName, message.payloadString);
-  }
-
-  setStatus("Connecting...");
-  _shouldReconnect = true;
-  _mqttClient.connect({
-    useSSL: true,
-    userName: _mqttCreds.username,
-    password: _mqttCreds.password,
-    timeout: 3,
-    onSuccess: onMQTTConnected,        
-    onFailure: onMQTTConnectionFailure
-  });
-}
-
-function MQTT_Disconnect() {
-  _shouldReconnect = false;
-  if (_mqttClient) {
-    try {
-      _mqttClient.disconnect();	
-    }
-    catch (ex) {}
-  }
-}
-
-// -------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // jQuery event wire-up
 $(document).ready(function() {
 
@@ -182,9 +124,7 @@ $(document).ready(function() {
       MQTT_Publish(`MtgRm/spk/ctrl/${spk}`, newStatus, 0, true);
     });
   }
-
 });
-
 
 $(document).ready(function() {
   _mqttCreds = localStorage.getItem("_mqttCreds");
@@ -193,3 +133,83 @@ $(document).ready(function() {
   }
   loadConnectionView();
 });
+
+// ----------------------------------------------------------------------------
+// Documentation:
+//   https://eclipse.dev/paho/index.php?page=clients/js/index.php
+//   https://eclipse.dev/paho/files/jsdoc/index.html
+
+let _mqttClient;
+let _mqttSubscriptions;
+
+function MQTT_Publish(topic, payload, qos=0, retained=false) {
+  const message = new Paho.MQTT.Message(payload);
+  message.destinationName = topic;
+  message.qos = qos;
+  message.retained = retained;
+  _mqttClient.send(message);
+}
+
+function MQTT_Subscribe(topic, cb) {
+  _mqttSubscriptions[topic] = cb;
+  _mqttClient.subscribe(topic, {qos: 0});
+}
+
+function MQTT_Connect() {
+  if (!_shouldReconnect) return;
+
+  _mqttClient = new Paho.MQTT.Client(_mqttCreds.host, 443, "myclientid_" + parseInt(Math.random() * 100, 10));
+  _mqttClient.onConnectionLost = onMQTTConnectionLost;
+
+  _mqttClient.onMessageArrived = function(message) {
+    for (const topic of Object.keys(_mqttSubscriptions)) {
+      _tryDeliverToSubscriber(topic, message.destinationName, message.payloadString);
+    }
+  }
+
+  function _tryDeliverToSubscriber(subTopic, msgTopic, msgPayload) {    
+    const subParts = subTopic.split("/");
+    const msgParts = msgTopic.split("/");
+    const captures = [];
+    for (let i=0; i<subParts.length; i++) {
+      const namePart = msgParts[i];
+      const topicPart = subParts[i];
+      if (topicPart === "#") {
+        for (; i<msgParts.length; i++) {
+          captures.push(msgParts[i]);
+        }
+        break;
+      } else if (topicPart === "+") {
+        captures.push(namePart);
+      } else if (namePart !== topicPart) {
+        return;
+      }
+    }
+    //console.log(subTopic, captures, msgTopic, msgPayload);
+    _mqttSubscriptions[subTopic](msgPayload, captures);
+  }
+
+  setStatus("Connecting...");
+  _shouldReconnect = true;
+  _mqttClient.connect({
+    useSSL: true,
+    userName: _mqttCreds.username,
+    password: _mqttCreds.password,
+    timeout: 3,
+    onSuccess: function() {
+      _mqttSubscriptions = {};
+      onMQTTConnected();
+    },
+    onFailure: onMQTTConnectionFailure
+  });
+}
+
+function MQTT_Disconnect() {
+  _shouldReconnect = false;
+  if (_mqttClient) {
+    try {
+      _mqttClient.disconnect();
+    }
+    catch (ex) {}
+  }
+}
